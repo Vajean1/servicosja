da3f8596b3fb610af0fd73ea1e99b8a514628912
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2025-11-10 20:24:41 -0300

    style: styled home att

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = 'servicosja';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`,
 })
