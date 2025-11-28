d9fb63a4375546e02cb454ec247bc563008d5648
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2025-11-28 18:37:25 -0300

    att

--- frontend/src/components/loginProviderPopup/loginProviderPopup.jsx
+++ frontend/src/components/loginProviderPopup/loginProviderPopup.jsx
@@ -43,7 +43,7 @@ export default function LoginProviderPopup ({open, close}) {
                         <form>
                             <input onChange={handleChangeLogin} name='email' type="email" placeholder='Email' />
                             <input onChange={handleChangeLogin} name='password' type="password" placeholder='Senha' />
-                            <button onClick={()=> navigate('/userPerfil')} type='submit'>Entrar</button>
+                            <button type='submit'>Entrar</button>
                             <a href="#">Esqueceu a senha?</a>
                         </form>
                             
