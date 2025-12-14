7106957f74a6515c5a74aee30e1c703e8b536d40
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2025-12-14 18:53:01 -0300

    alteraçoes nos planos

--- frontend/src/pages/plans/plans.jsx
+++ frontend/src/pages/plans/plans.jsx
@@ -8,17 +8,18 @@ export default function Plans () {
 
             <section className={styles.planoGrid}>
                 <div className={styles.cardPlano}>
-                    <p className={styles.pOculto}>Plano mais popular</p>
+                    <p className={styles.pOculto}>Ideal para iniciantes</p>
                     <div className={styles.cardPrincipal}>
                         <h2>Básico</h2>
                         <p className={styles.frasePlano}>Ideal para iniciantes</p>
                         <h1>Grátis</h1>
                         <ul>
-                            <li className={styles.detalheOk}>Visualização de Pedidos: 5x / semana</li>
-                            <li className={styles.detalheOk}>Área de Atuação: 5km.</li>
-                            <li className={styles.detalheX}>Anúncios rotativos</li>
-                            <li className={styles.detalheX}>Selo de Verificação JÁ</li>
-                            <li className={styles.detalheX}>Agenda Digital JÁ (Google Calendar)</li>
+                            <li className={styles.detalheOk}>Visibilidade basica na busca</li>
+                            <li className={styles.detalheOk}>Área de Atuação até 5km.</li>
+                            <li className={styles.detalheOk}>5 contatos liberados por mês</li>
+                            <li className={styles.detalheX}>Chatbot IA</li>
+                            <li className={styles.detalheX}>Selo Profissional 24h</li>
+                            <li className={styles.detalheX}>Geolocalização prioritária</li>
                         </ul>
                         <button className={styles.buttonPlano}>Selecionar Plano</button>
                         <span className={styles.spanOculto}>ou consulte-nos</span>
@@ -26,17 +27,18 @@ export default function Plans () {
                 </div>
 
                 <div className={styles.cardPlano}>
-                    <p className={styles.pOculto}>Plano mais popular</p>
+                    <p className={styles.pOculto}>Ideal para quem está crescendo</p>
                     <div className={styles.cardPrincipal}>
                         <h2>Intermediário</h2>
                         <p className={styles.frasePlano}>Ideal para quem está crescendo</p>
-                        <h1>$39,90<span className={styles.valorDestaque}>/Mês</span></h1>
+                        <h1>$29,90<span className={styles.valorDestaque}>/Mês</span></h1>
                         <ul>
-                            <li className={styles.detalheOk}>Visualização de Pedidos: Ilimitada (Grátis)</li>
-                            <li className={styles.detalheOk}>Área de Atuação: 15km ou seleção de até 3 bairros.</li>
-                            <li className={styles.detalheOk}>Anúncios rotativos</li>
-                            <li className={styles.detalheOk}>Selo de Verificação JÁ</li>
-                            <li className={styles.detalheX}>Agenda Digital JÁ (Google Calendar)</li>
+                            <li className={styles.detalheOk}>Visibilidade padrão na busca</li>
+                            <li className={styles.detalheOk}>Área de Atuação até 15km.</li>
+                            <li className={styles.detalheOk}>20 contatos liberados por mês</li>
+                            <li className={styles.detalheOk}>Chatbot IA</li>
+                            <li className={styles.detalheX}>Selo Profissional 24h</li>
+                            <li className={styles.detalheX}>Geolocalização prioritária</li>
                         </ul>
                         <button className={styles.buttonPlano}>Selecionar Plano</button>
                         <span className={styles.spanOculto}>ou consulte-nos</span>
@@ -44,17 +46,18 @@ export default function Plans () {
                 </div>
 
                 <div className={styles.cardPlano}>
-                    <p className={styles.pOculto}>Plano mais popular</p>
+                    <p className={styles.pOculto}>Ideal para quem é exclusivo</p>
                     <div className={styles.cardPrincipal}>
                         <h2>Profissional</h2>
                         <p className={styles.frasePlano}>Ideal para quem é exclusivo</p>
-                        <h1>$79,90<span className={styles.valorDestaque}>/Mês</span></h1>
+                        <h1>$59,90<span className={styles.valorDestaque}>/Mês</span></h1>
                         <ul>
-                            <li className={styles.detalheOk}>Visualização de Pedidos: Ilimitada (Grátis)</li>
-                            <li className={styles.detalheOk}>Área de Atuação: Ilimitada.</li>
-                            <li className={styles.detalheOk}>Anúncios fixos em 4 das categorias.</li>
-                            <li className={styles.detalheOk}>Selo de Verificação JÁ</li>
-                            <li className={styles.detalheOk}>Agenda Digital JÁ (Google Calendar)</li>
+                            <li className={styles.detalheOk}>Visibilidade Otimizada</li>
+                            <li className={styles.detalheOk}>Área de Atuação até 15km.</li>
+                            <li className={styles.detalheOk}>Acesso ilimitado a pedidos.</li>
+                            <li className={styles.detalheOk}>Chatbot IA</li>
+                            <li className={styles.detalheOk}>Selo Profissional 24h</li>
+                            <li className={styles.detalheOk}>Geolocalização prioritária</li>
                         </ul>
                         <button className={styles.buttonPlano}>Selecionar Plano</button>
                         
